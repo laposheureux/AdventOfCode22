e6e7b09154cae855e1c4356e5e0765c975e39826
Aaron L'Heureux <laposheureux@users.noreply.github.com>
2022-12-01 19:08:21 -0800

    Cleanup day 1

--- Day1.swift
+++ Day1.swift
@@ -8,15 +8,10 @@ import Foundation
 ​
 final class Day1: Day {
     func part1(_ input: String) -> CustomStringConvertible {
-        var largestIndexValue: (Int, Int) = (-1, -1)
-        _ = input
+        return input
             .split(separator: "\n", omittingEmptySubsequences: false)
             .reduce(into: [0]) { partialResult, calories in
                 guard !calories.isEmpty else {
-                    let elfTotal = partialResult[partialResult.count - 1]
-                    if elfTotal > largestIndexValue.1 {
-                        largestIndexValue = (partialResult.count - 1, elfTotal)
-                    }
                     partialResult.append(0)
                     return
                 }
@@ -27,12 +22,11 @@ final class Day1: Day {
 ​
                 partialResult[partialResult.count - 1] += intCalories
             }
-​
-        return largestIndexValue.1
+            .max() ?? 0
     }
 ​
     func part2(_ input: String) -> CustomStringConvertible {
-        let topThreeElfCalories = input
+        return input
             .split(separator: "\n", omittingEmptySubsequences: false)
             .reduce(into: [0]) { partialResult, calories in
                 guard !calories.isEmpty else {
@@ -47,8 +41,6 @@ final class Day1: Day {
                 partialResult[partialResult.count - 1] += intCalories
             }
             .max(count: 3)
-            .reduce(0, +)
-​
-        return topThreeElfCalories
+            .sum
     }
 }
